71a0cb77c4afe64aaf10c7340dbb2ec2aefd4b83
pratiknawale93 <pratiknawale93@gmail.com>
2026-05-03 15:27:56 +0530

    Add files via upload

---
+++ Random_no.js
@@ -0,0 +1,24 @@
+ const max=prompt("Write the range of max no");
+
+ const no = Math.floor(Math.random()*max) + 1;
+ let guses=prompt("Enter your no");
+ while(true)
+ {
+    if(guses=="quit")
+    {
+        console.log("The game is Quitting");
+        break;
+    }
+    if(guses==no)
+    {
+        console.log("Congratulations !");
+        break;
+    }
+    else if(guses <no)
+    {
+        guses=prompt(" Hint : you no was too small try again !");
+    }
+    else{
+        guses=prompt(" Hint :your no was too large try again !");
+    }
+ }
